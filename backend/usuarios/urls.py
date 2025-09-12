54a4117a627f362b307690099df22c7737e842b0
jhonatangrajales <tu-email>
2025-09-12 14:03:50 -0500

    se corrige inicio de sesion y se agrega url del backend

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, RegisterView, TimezoneView
+from .views import LoginView, RegisterView, TimezoneView, UserDetailView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('timezones/', TimezoneView.as_view(), name='timezones'),
+    path('auth/user/', UserDetailView.as_view(), name='user_detail'),
 ]
--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer, MyTokenObtainPairSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 import pytz
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -34,3 +34,10 @@ class LoginView(APIView):
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
