3504e79aa94c900680192843a030d3a16c9f6465
jhonatangrajales <tu-email>
2025-09-11 22:58:02 -0500

    se agrega ip elastica de aws al settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '54.164.220.16']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '98.81.85.217']
 
 
 # Application definition
