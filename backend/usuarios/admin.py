bb39722f90e040e82b925030faea7a8da49cfc65
jhonatangrajales <tu-email>
2025-09-25 19:20:20 -0500

    se corrige error al inciar sesion

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import PerfilUsuario
 
+
 @admin.register(PerfilUsuario)
 class PerfilUsuarioAdmin(admin.ModelAdmin):
-    list_display = ('user', 'sede')
-    search_fields = ('user__username', 'sede__nombre')
-    filter_horizontal = ('sedes_administradas',) # To manage ManyToManyField
+    list_display = ('user', 'get_sede_nombre', 'organizacion')
+    search_fields = ('user__username', 'sede__nombre', 'organizacion__nombre')
+    list_filter = ('sede', 'organizacion')
+    filter_horizontal = ('sedes_administradas',)
+
+    def get_sede_nombre(self, obj):
+        """
+        Devuelve el nombre de la sede o un valor por defecto si no existe.
+        """
+        if obj.sede:
+            return obj.sede.nombre
+        return "Sin sede asignada"
+    get_sede_nombre.short_description = 'Sede'
+    get_sede_nombre.admin_order_field = 'sede__nombre'
