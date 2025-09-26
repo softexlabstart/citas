ec22c906e7cc66f3c3bac3c5d13bcd762d99ed78
jhonatangrajales <tu-email>
2025-09-25 19:32:14 -0500

    se corrige error al inciar sesion

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -17,4 +17,3 @@ class PerfilUsuarioAdmin(admin.ModelAdmin):
             return obj.sede.nombre
         return "Sin sede asignada"
     get_sede_nombre.short_description = 'Sede'
-    get_sede_nombre.admin_order_field = 'sede__nombre'
